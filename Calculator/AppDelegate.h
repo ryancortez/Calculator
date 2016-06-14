91e3ee593c52aee29879ce2600458bfc1c9f2aee
ryancortez <ryancortez@mac.com>
2016-06-14 12:42:57 -0500

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Ryan Cortez on 6/14/16.
+//  Copyright © 2016 Ryan Cortez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
