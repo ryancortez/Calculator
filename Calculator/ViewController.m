82202e29ded7f0509cbe412f01849fb38e31f58a
ryancortez <ryancortez@mac.com>
2016-06-14 13:48:09 -0500

    Finished Assignment 2 for June 2016 The Iron Yard

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -9,19 +9,51 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *firstNumberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *secondNumberTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultsLabel;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(double)addNumbersWithFirstNumber:(double)firstNumber andSecondNumber: (double) secondNumber {
+    double results = 0;
+    results = firstNumber + secondNumber;
+    return results;
+}
+-(double)subtractNumbersWithFirstNumber:(double)firstNumber andSecondNumber: (double) secondNumber {
+    double results = 0;
+    results = firstNumber - secondNumber;
+    return results;
+}
+-(double)multiplyNumbersWithFirstNumber:(double)firstNumber andSecondNumber: (double) secondNumber {
+    double results = 0;
+    results = firstNumber * secondNumber;
+    return results;
+}
+-(double)divideNumbersWithFirstNumber:(double)firstNumber andSecondNumber: (double) secondNumber {
+    double results = 0;
+    results = firstNumber / secondNumber;
+    return results;
+}
+
+- (IBAction)addButton:(id)sender {
+    self.resultsLabel.text = [NSString stringWithFormat:@"%f", [self addNumbersWithFirstNumber:self.firstNumberTextField.text.doubleValue andSecondNumber:self.secondNumberTextField.text.doubleValue]];
+}
+- (IBAction)subtractButton:(id)sender {
+    self.resultsLabel.text = [NSString stringWithFormat:@"%f", [self subtractNumbersWithFirstNumber:self.firstNumberTextField.text.doubleValue andSecondNumber:self.secondNumberTextField.text.doubleValue]];
+}
+- (IBAction)multiplyButton:(id)sender {
+    self.resultsLabel.text = [NSString stringWithFormat:@"%f", [self multiplyNumbersWithFirstNumber:self.firstNumberTextField.text.doubleValue andSecondNumber:self.secondNumberTextField.text.doubleValue]];
+}
+- (IBAction)divideButton:(id)sender {
+    self.resultsLabel.text = [NSString stringWithFormat:@"%f", [self divideNumbersWithFirstNumber:self.firstNumberTextField.text.doubleValue andSecondNumber:self.secondNumberTextField.text.doubleValue]];
 }
 
 @end
